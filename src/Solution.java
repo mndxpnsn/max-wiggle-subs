55d46aaf8533094a248cb3643ca5f01f3c952aa7
derek <harrison.derek.w@gmail.com>
2022-08-20 01:01:58 +0200

    add comments and refactor

--- src/Solution.java
+++ src/Solution.java
@@ -14,15 +14,18 @@ class Solution {
         int res = 0;
 
         int n = nums.length;
-        int num = bound_up ? 1 : 0;
+
+        // Get data from memo table if available
         if(dp[i] != 0) {
             return dp[i];
         }
 
+        // Indices are out of bounds
         if(i > n - 1) {
             return 0;
         }
 
+        // End of array reached
         if(i == n - 1) {
             if(bound_up && nums[i] < prev) {
                 return 1;
@@ -32,23 +35,21 @@ class Solution {
             }
         }
 
+        // Compute max wiggle subsequence
         if(i < n - 1) {
-            if(bound_up) {
-                if(nums[i] < prev) {
-                    int val1 = 1 + max_wiggle_rec(nums, false, nums[i], i + 1, dp);
-                    int val2 = max_wiggle_rec(nums, true, prev, i + 1, dp);
-                    res = max(val1, val2);
-                }
+            if(bound_up && nums[i] < prev) {
+                int val1 = 1 + max_wiggle_rec(nums, false, nums[i], i + 1, dp);
+                int val2 = max_wiggle_rec(nums, true, prev, i + 1, dp);
+                res = max(val1, val2);
             }
-            if(!bound_up) {
-                if(nums[i] > prev) {
-                    int val1 = 1 + max_wiggle_rec(nums, true, nums[i], i + 1, dp);
-                    int val2 = max_wiggle_rec(nums, false, prev, i + 1, dp);
-                    res = max(val1, val2);
-                }
+            if(!bound_up && nums[i] > prev) {
+                int val1 = 1 + max_wiggle_rec(nums, true, nums[i], i + 1, dp);
+                int val2 = max_wiggle_rec(nums, false, prev, i + 1, dp);
+                res = max(val1, val2);
             }
         }
 
+        // Store data in memo table
         dp[i] = res;
 
         return res;
@@ -77,22 +78,22 @@ class Solution {
         int start_low = 0;
         int start_high = 0;
 
+        // Check if all numbers are the same
         if(all_same(nums)) {
             return 1;
         }
 
+        // Check if size of input array is 1
         if(n == 1) {
             return 1;
         }
 
-        if(n == 2 && nums[0] == nums[1]) {
-            return 1;
-        }
-
+        // Case input array is size two and elements are not equal
         if(n == 2 && nums[0] != nums[1]) {
             return 2;
         }
 
+        // Compute first number greater than the value of first element
         for(int i = 0; i < n; ++i) {
             start_high = i;
             if(nums[i] > nums[0]) {
@@ -100,10 +101,12 @@ class Solution {
             }
         }
 
+        // Compute the index of the end of the increasing subarray starting from start_high
         while(start_high + 1 < n && nums[start_high + 1] >= nums[start_high]) {
             start_high++;
         }
 
+        // Compute first number less than the value of first element
         for(int i = 0; i < n; ++i) {
             start_low = i;
             if(nums[i] < nums[0]) {
@@ -111,10 +114,12 @@ class Solution {
             }
         }
 
+        // Compute the index of the end of the decreasing subarray starting from start_low
         while(start_low + 1 < n && nums[start_low + 1] <= nums[start_low]) {
             start_low++;
         }
 
+        // Compute the maximum wiggle subsequence
         int val1 = 2 + max_wiggle_rec(nums, true, nums[start_high], start_high + 1, dp);
         int val2 = 2 + max_wiggle_rec(nums, false, nums[start_low], start_low + 1, dp);
 
